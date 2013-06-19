a9951435aa00b473662f23b6e90827307cee9420
fisker.cui <fisker.cui@gmail.com>
2013-06-19 14:51:18 +0800

    删除FrameWork

--- FrameWork/DataLib/test/com/road/pitaya/dao/IPlayerDao.java
+++
@@ -1,9 +0,0 @@
-package com.road.pitaya.dao;
-
-import com.road.pitaya.database.IBaseDao;
-import com.road.pitaya.entity.Player;
-
-public interface IPlayerDao extends IBaseDao<Player>
-{
-
-}
--- FrameWork/GameServer/src/com/road/dota/command/ProtocolOut.java
+++
@@ -1,9 +0,0 @@
-package com.road.dota.command;
-
-
-public final class ProtocolOut 
-{
-    public static final short USER_LOGIN = 0x001; // 测试
-    public static final short USER_UPDATE_BAG = 0x002; // 测试
-}
-
